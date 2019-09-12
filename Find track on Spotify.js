e812b5cb9f47b146532f72c0be79b89b79a906e9
David Metcalfe <DavidMetcalfe@users.noreply.github.com>
2019-09-12 01:44:04 -0700

    Updating Spotify search URL.

--- Find track on Spotify.js
+++ Find track on Spotify.js
@@ -3,5 +3,5 @@ javascript: (function() {
   song = song.replace(/amp;/g, "");
   var artist = document.querySelector("#player-artist").innerHTML;
   var text = encodeURIComponent(artist + " - " + song);
-  window.open("https://open.spotify.com/search/results/" + text);
+  window.open("https://open.spotify.com/search/" + text);
 })();
